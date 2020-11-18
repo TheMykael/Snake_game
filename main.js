928d4bf3685dda3cd51f7b2cba33335cde5fa627
unknown <maycontalmeida@gmail.com>
2020-11-18 10:52:29 -0300

    Movimentos iniciais da cobrinha

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ snake[0] = {
 	x: 8 * box,
 	y: 8 * box
 }
+let direction = "right";
 
 function criarBG(){
 	context.fillStyle = "lightgreen";
@@ -19,5 +20,26 @@ function criarCobrinha(){
 	}
 }
 
-criarBG();
-criarCobrinha();
+function iniciarJogo(){
+	criarBG();
+	criarCobrinha();
+	// Posição x e y da cobrinha, para ela ter um ponto de partida
+	let snakeX = snake[0].x;
+	let snakeY = snake[0].y;
+	// Coordenadas da cobrinha, onde ela irá seguir
+	if(direction == "right") snakeX += box;
+	if(direction == "left") snakeX -= box;
+	if(direction == "up") snakeY += box;
+	if(direction == "down") snakeY -= box;
+	// Função "pop" para retirar o último elemento do nosso array
+	snake.pop();
+	// É necessário criar a cabeça da cobrinha com o "unshift" que irá acrescentar à frente do elemento
+	let newHead = {
+		x: snakeX,
+		y: snakeY
+	}
+	snake.unshift(newHead);
+}
+
+// Isso serve para atualizar o jogo. O padrão sugerido foi de 100ms
+let jogo = setInterval(iniciarJogo, 100);
